72206043961ebfc9657ae48d42499d047b9c0478
zhangzhangzhang158 <1755890794@qq.com>
2019-04-28 16:03:07 +0800

    添加监听事件

--- animate.js
+++ animate.js
@@ -2,3 +2,15 @@
 function getElem(obj) {
     return document.getElementById(obj)
 }
+
+
+/*添加监听事件*/
+function addEL(ele,type,fn) {
+    if (ele.addEventListener) {
+        ele.addEventListener(type,fn,false)
+    }else if (ele.attachEvent) {
+        ele.attachEvent("on"+type,fn)
+    }else {
+        ele["on"+type]=fn;
+    }
+}
